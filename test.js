f5c70fabc5b02979540f2e54c9fa46c5f73cd330
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-08-05 14:14:48 +0200

    Work

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 require("dotenv/config");
+
+function test(secret) {
+
+    const secretMatches = process.env.MEANING_OF_LIFE === secret;
+    console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+    console.log(`The secret does ${secretMatches ? "" : "not "}match the meaning of life`);
+    return secretMatches;
+
+};
+
+test("42");
